36fce56465d3e4601d7123542377ede469238760
Benedetta Mestichelli <benedetta.mestichelli@gssi.it>
2026-05-23 12:20:15 +0200

    Clarify par_bin_file option in parameters.py
    Updated par_bin_file comment to clarify its purpose.

--- parameters.py
+++ parameters.py
@@ -4,7 +4,7 @@ binaries_yn = True
 multiples = True #Add "fake" triplets to correct the binary fraction
 
 plotta = True #plot stellar and binary distributions
-par_bin_file=True
+par_bin_file=True #print file with new orbital parameters ("binaries_new.dat")
 
 change_imf = False
 verbose = True
